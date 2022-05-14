bf6c9f9af1c5d07c856f91889b99d76f5f962c74
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-05-14 16:00:52 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class Exercise1 {
 
 	public static void main(final String[] args) {
-
 		Exercise1 obj = new Exercise1();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int n = sc.nextInt();
-			System.out.println(n);
-			System.out.println(n/0);
 			System.out.println(obj.esCuadradoPerfecto(n));			
 		}
 	}
@@ -19,9 +16,8 @@ public class Exercise1 {
 	public boolean esCuadradoPerfecto(int numero) {
 		//Significa que luego de una llamada recursiva, el numero es divisible por un cuadrado perfecto
 		if (numero == 1) return true;
-		/* Se sabe que numero es cuadrado perfecto si es multiplicacion de 2 numeros naturales
-		 * o el de otros 2 cuadrados perfectos (n^2 = a^2 * b^2)
-		*/
+		/* Se sabe que numero es cuadrado perfecto si es producto de 2 numeros naturales
+		 * o el de otros 2 cuadrados perfectos (n^2 = a^2 * b^2)*/
 		for (int a = 2; Math.pow(a, 2) <= numero; a++)
 			// Probamos si "numero" es divisble por algun cuadrado perfecto menor o igual a el mismo
 			if(numero % Math.pow(a, 2) == 0)
