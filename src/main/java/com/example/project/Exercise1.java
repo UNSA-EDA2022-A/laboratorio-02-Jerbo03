0a3c3a29269aca8edfd075004ad00a3a2187b87b
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-05-14 15:52:37 -0500

    Quitar tildes

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,15 +15,15 @@ public class Exercise1 {
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-		//Significa que luego de una llamada recursiva, el número es divisible por un cuadrado perfecto
+		//Significa que luego de una llamada recursiva, el numero es divisible por un cuadrado perfecto
 		if (numero == 1) return true;
-		/* Se sabe que numero es cuadrado perfecto si es multiplicación de 2 números naturales
+		/* Se sabe que numero es cuadrado perfecto si es multiplicacion de 2 numeros naturales
 		 * o el de otros 2 cuadrados perfectos (n^2 = a^2 * b^2)
 		*/
 		for (int a = 2; Math.pow(a, 2) <= numero; a++)
-			// Probamos si "numero" es divisble por algún cuadrado perfecto menor o igual a él mismo
+			// Probamos si "numero" es divisble por algun cuadrado perfecto menor o igual a el mismo
 			if(numero % Math.pow(a, 2) == 0)
-				// Si es divisble, hay que probar si el cociente (b) también cuadrado perfecto
+				// Si es divisble, hay que probar si el cociente (b) tambien cuadrado perfecto
 				return esCuadradoPerfecto((int) (numero/Math.pow(a, 2)));
 		return false;
 	}
